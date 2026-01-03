5d5badc347a399f7db0a8990b9fba73974577632
nkxrfxforum <nkxrfxforum@gmail.com>
2026-01-03 20:08:05 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-const GCF_URL = 'https://wwechoscoringtoolsweb-547324461314.asia-east1.run.app';
+var GCF_URL = 'https://wwechoscoringtoolsweb-547324461314.asia-east1.run.app';
+
+var CONFIG = {
+    SIMILARITY_THRESHOLD: 0.7,
+    CHAIN_THRESHOLD: 0.8,
+    BINARIZE_THRESHOLD: 128,
+    EXPECTED_WIDTH: 1920,
+    EXPECTED_HEIGHT: 1080,
+    MAX_FILE_SIZE: 10 * 1024 * 1024
+};
